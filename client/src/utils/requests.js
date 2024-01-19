950b770ea70579445dc466c60e00096cb1e8d9ab
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-01-20 02:37:52 +0530

    committed tweet filter accn to date event

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -1,6 +1,5 @@
 export function fetchData(drugname, setVariable, setError, setLoading) {
     fetch(`http://localhost:9000/testAPI/${drugname}`).then((res) => res.json()).then((data) => {
-
         setVariable(data);
         console.log(data);
         setLoading(false);
